655f6df1644128f29ebc03e6fafa061faa757017
Guilherme Decampo <guilherme.decampo@gmail.com>
2014-04-13 06:14:38 -0300

    Fix filters router

--- client/views/common/loginButtons.js
+++ client/views/common/loginButtons.js
@@ -4,3 +4,9 @@ Template.loginButtons.rendered = function () {
   $('.login-display-name').addClass('hidden');
 };
 
+Template._loginButtonsLoggedOutSingleLoginButton.rendered = function () {
+  $('#login-buttons-logout').text('Sair');
+  $('.text-besides-image').text('Entrar com Facebook');
+  $('.login-display-name').addClass('hidden');
+};
+
