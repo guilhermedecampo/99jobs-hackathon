f87bb01f6dec3a195a0cc8814796c7ba75a4a0b7
Guilherme Decampo <guilherme.decampo@gmail.com>
2014-04-13 05:45:02 -0300

    Change copy loginButtons

---
+++ client/views/common/loginButtons.js
@@ -0,0 +1,6 @@
+Template.loginButtons.rendered = function () {
+  $('#login-buttons-logout').text('Sair');
+  $('.text-besides-image').text('Entrar com Facebook');
+  $('.login-display-name').addClass('hidden');
+};
+
