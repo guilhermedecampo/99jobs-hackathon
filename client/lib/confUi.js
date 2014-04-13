92f0db92f72da836d0553063e0233a1edd677dbd
Guilherme Decampo <guilherme.decampo@gmail.com>
2014-04-13 08:05:09 -0300

    Add events

---
+++ client/lib/confUi.js
@@ -0,0 +1,20 @@
+
+
+//Divide x / y
+
+UI.registerHelper('picFacebook', function(facebookId) {
+  var user = Meteor.user();
+  if (user&& user.profile && user.profile.id) {
+    return 'http://graph.facebook.com/'+facebookId + '/picture/?width=200&height=200';
+  } else {
+    return 'images/profile-photo.jpg';
+  }
+});
+
+Meteor.subscribe('events');
+
+
+UI.registerHelper('clean', function(val) {
+  return val.replace(/ /g, "");
+
+});
